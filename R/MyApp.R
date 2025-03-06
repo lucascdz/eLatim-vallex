0d75a8b21b328d019de851ea3caa4504ebb9e5d7
Lucas Dezotti <lucascdz@gmail.com>
2025-03-06 12:44:54 -0300

    Output filtered by lemma

--- R/MyApp.R
+++ R/MyApp.R
@@ -1,30 +1,16 @@
+library(data.table)
 library(shiny)
 
-{
-   DataFrame <- read.csv('./data/VallencyDataframeLT.tsv',sep='\t')
-   #
-   lemmas <- unique(DataFrame$lemmaHEAD)
-   lemmas <- lemmas[order(lemmas)]
-   lemmas[lemmas==''] <- '--'
-   #
-   feats <- unique(DataFrame$arg_feat)
-   feats <- feats[order(feats)]
-   feats <- c('--',feats)
-   #
-   forms <- unique(DataFrame$arg_form)
-   forms <- forms[order(forms)]
-   forms <- c('--',forms)
-   #
-   funcs <- unique(DataFrame$arg_func)
-   funcs <- funcs[order(funcs)]
-   funcs <- c('--',funcs)
-}
+DataFrame <- read.csv('./data/VallencyDataframeLT.tsv',sep='\t')
+lemmas <- unique(DataFrame$lema_regente)
+lemmas <- lemmas[order(lemmas)]
+lemmas[lemmas==''] <- '--'
 
 ui <- fluidPage(
    
    titlePanel("eLatim-vallex v.0.5",windowTitle = "eLatim-vallex v.0.5"),
    
-   tabPanel("Dictionary",
+   tabPanel("eLatim-vallex",
             sidebarLayout(
                sidebarPanel(
                   width=3,
@@ -34,9 +20,9 @@ ui <- fluidPage(
                   selectInput("headword", "lema", choices = lemmas),
                   hr(),
                   code('DEPENDENTE'),
-                  selectInput("arg_feat", "forma do sintagma", choices = feats),
-                  selectInput("arg_form", "forma da palavra", choices = forms),
-                  selectInput("arg_func", "relação de dependência", choices = funcs),
+                  selectInput("sintagma", "sintagma", choices = '--'),
+                  selectInput("traco", "traço", choices = '--'),
+                  selectInput("funcao", "função", choices = '--'),
                   actionButton('reset','reiniciar')
                   #checkboxInput()
                   #sliderInput()
@@ -47,7 +33,7 @@ ui <- fluidPage(
                          tags$br(),
                          tags$br(),
                          tags$br(),
-                         htmlOutput('title_examples'),
+                         htmlOutput('Exemplos'),
                          DT::dataTableOutput('examples'),
                          tags$br(),
                          tags$br(),
@@ -64,10 +50,10 @@ server <- function(input, output,session) {
    # reset button ####
    observeEvent(input$reset, {
       updateSelectInput(session, "headwordText", selected = '')
-      updateSelectInput(session, "headword", choices = lemmas, selected = '--')
-      updateSelectInput(session, "arg_feat", choices = feats, selected = '--')
-      updateSelectInput(session, "arg_form", choices = forms, selected = '--')
-      updateSelectInput(session, "arg_func", choices = funcs, selected = '--')
+      updateSelectInput(session, "headword", choices = lemmas)
+      updateSelectInput(session, "sintagma", choices = '--')
+      updateSelectInput(session, "funcao", choices = '--')
+      updateSelectInput(session, "traco", choices = '--')
    })
    
    
@@ -77,10 +63,11 @@ server <- function(input, output,session) {
          if(!is.null(input$headwordText) && nchar(input$headwordText)>0){
             lemmaChoices <- lemmas[str_detect(lemmas,input$headwordText)]
             lemmaChoices <- c(lemmaChoices,'--')
+            updateSelectInput(session, "headword", choices=lemmaChoices)
          } else {
             lemmaChoices <- lemmas
+            updateSelectInput(session, "headword", choices=lemmaChoices)
          }
-         updateSelectInput(session, "headword", choices=lemmaChoices)
       })
    
    
@@ -88,31 +75,39 @@ server <- function(input, output,session) {
    
    # filter choices ####
    observeEvent(
-      c(input$headword,input$arg_func,input$arg_feat,input$arg_form),{
-         
-         if(input$headword!='--' & input$arg_func=='--' & input$arg_feat=='--' & input$arg_form=='--'){
+      c(input$headword,input$sintagma,input$funcao,input$traco),{
+         if(input$headword!='--'){
+            df <- DataFrame[DataFrame$lema_regente==input$headword,]
             
-            df <- DataFrame[DataFrame$lemmaHEAD==input$headword,]
+            sintagmas <- unique(df$sintagma)
+            sintagmas <- sintagmas[order(sintagmas)]
+            sintagmas <- c('TODOS',sintagmas,'--')
+            updateSelectInput(session, 'sintagma', choices=sintagmas)
             #
-            #lemmaChoices <- lemmas[lemmas %in% df$lemmaHEAD]
-            #lemmaChoices <- c('--',lemmaChoices)
-            #updateSelectInput(session, "headword", choices=lemmaChoices)
+            funcoes <- unique(df$funcao)
+            funcoes <- funcoes[order(funcoes)]
+            funcoes <- c('TODOS',funcoes,'--')
+            updateSelectInput(session, 'funcao', choices=funcoes)
             #
-            featChoices <- feats[feats %in% df$arg_feat]
-            featChoices <- c('--',featChoices)
-            updateSelectInput(session, "arg_feat", choices=featChoices)
+            tracos <- unique(df$traco)
+            tracos <- tracos[order(tracos)]
+            tracos[tracos==''] <- 'TODOS'
+            tracos <- c(tracos,'--')
+            updateSelectInput(session, 'traco', choices=tracos)
             #
-            formChoices <- forms[forms %in% df$arg_form]
-            formChoices <- c('--',formChoices)
-            updateSelectInput(session, "arg_form", choices=formChoices)
-            #
-            funcChoices <- funcs[funcs %in% df$arg_func]
-            funcChoices <- c('--',funcChoices)
-            updateSelectInput(session, "arg_func", choices=funcChoices) 
-            
+         } else {
+            #ok
          }
-         
       })
+   
+   # output ####
+   observeEvent(
+      c(input$headword,input$sintagmas,input$tracos,input$funcoes),{
+         df <- DataFrame[DataFrame$lema_regente==input$headword,3:5]
+         df <- df[!duplicated(df),]
+         output$examples <- DT::renderDataTable(data.table(df))
+      })
+   
    
    
 }
