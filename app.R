0c17827539f26ca68a6bd58e77381f6e63de9caf
Lucas Dezotti <lucascdz@gmail.com>
2025-03-07 17:15:13 -0300

    Enclose app with runApp()

--- app.R
+++ app.R
@@ -1,11 +1,12 @@
 # eLatim-vallex
 
-## install required packages
+## check required packages
 requirements <- c('shiny','readtext','tidyverse','DT')
 install.packages(requirements[!requirements %in% as.data.frame(installed.packages(),stringsAsFactors = F)[['Package']]])
+
 ## run app
 source('./R/MyApp.R')
-MyApp('./data/DataFrame.tsv')
+runApp(MyApp('./data/DataFrame.tsv'),launch.browser = T)
 
 
 # For updating data sources, uncomment these lines:
